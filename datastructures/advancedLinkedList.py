dd5f1812895d0155d850ac9c273f37b925a66ede
Nnamaka <nnamaka7@gmail.com>
2024-01-01 19:42:44 +0100

    added note to understand double linked list

--- datastructures/advancedLinkedList.py
+++ datastructures/advancedLinkedList.py
@@ -1,3 +1,13 @@
+
+
+"""
+NOTE: This is a double linked list and the first and the last element should point
+to each other.
+also if you're having issues understanding the kind of stack operation of how elements are
+added and removed from the list, you can ask gpt/berd to implement an equivalent of a "push"
+function in python's "list" class to see if items are added at the front or the back
+"""
+
 class Node:
     def __init__(self, data) -> None:
         self.data = data
