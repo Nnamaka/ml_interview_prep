aa40b935f6581161f30d695f0b122ab140bd5e0e
Nnamaka <nnamaka7@gmail.com>
2024-01-15 20:54:03 +0100

    safe

--- datastructures/studyhashtable.py
+++ datastructures/studyhashtable.py
@@ -7,6 +7,7 @@
 # subsequent videos on coding other datastructures will be released too
 # my youtube channel : https://www.youtube.com/channel/UCiiY1pXCt0Msao1AFlydAbQ
 # The channel focuses on software engineering and machine learning engineering.
+# copyright 2024
 
 class HashTable:
     def __init__(self, length=4) -> None:
