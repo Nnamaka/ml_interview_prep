9aa0e375d6d43543e341685d3878750c51b81e65
Nnamaka <nnamaka7@gmail.com>
2024-01-19 22:25:43 +0100

    binary tree

---
+++ datastructures/binarytree.py
@@ -0,0 +1,29 @@
+# implementation of binary tree datastructure
+
+class Node:
+    def __init__(self, data) -> None:
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+    
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data)
+        if self.right:
+            self.right.printTree()
