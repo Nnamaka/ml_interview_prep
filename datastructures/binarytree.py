7fb2452df5dbe4b0c9ac1eb8ca607863b225c761
Nnamaka <nnamaka7@gmail.com>
2024-04-06 14:48:36 +0100

    trasversing binary trees

--- datastructures/binarytree.py
+++ datastructures/binarytree.py
@@ -28,3 +28,54 @@ class Node:
         print(self.data)
         if self.right:
             self.right.printTree()
+
+    # in-order trasversal
+    def inorderTrasversal(self, root):
+        res = []
+
+        if root:
+            res = self.inorderTrasversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTrasversal(root.right)
+        return res
+        
+    # pre-order trasversal
+    def preorderTrasversal(self, root):
+        res = []
+
+        if root:
+            res.append(root.data)
+            res = res + self.preorderTrasversal(root.left)
+            res = res + self.preorderTrasversal(root.right)
+        return res
+    
+    # post-order trasversal
+    def postorderTrasversal(self, root):
+        res = []
+
+        if root:
+            # left, right, root
+            res = res + self.postorderTrasversal(root.left)
+            res = res + self.postorderTrasversal(root.right)
+            res.append(root.data)
+        return res
+    
+# create root node
+root = Node(12)
+
+# insert data to nodes
+root.insert(6)
+root.insert(14)
+root.insert(3)
+
+# print elements of a binary list
+root.printTree()
+
+root.insert(19)
+root.insert(31)
+root.insert(42)
+
+# trasversing a list
+print(root.inorderTrasversal(root))
+print(root.preorderTrasversal(root))
+print(root.postorderTrasversal(root))
